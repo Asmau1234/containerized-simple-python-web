00dc055bd12d17eb9be5c59ec4ba62e2ea4a2bcd
amaliyu <amaliyu@swglobal.com>
2018-09-27 13:39:17 +0100

    adding files

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app= Flask()[D__name__)
+
+@app.rout()[D[De[(""[D/")
+def main():
+return ""[DWelcome!["
+
+@app.route()[D["/how are you'[D[D[D[D[D[D[D[D[D[D[D[D['[C[C[C[C[C[C[C[C[C[C[C[C[C[C)[C'[C[D[[C[[C')
+def hello():
+return ''[DI am good, how about you?'
+
+if __name__ == "__main__"
+app.run()
